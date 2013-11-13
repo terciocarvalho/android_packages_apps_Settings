0fefb52bcae1ff6eabd23e74b72767630e1476f5
cristianomatos <cristianobmatos@gmail.com>
2013-11-13 19:13:23 -0200

    AOKP custom system animations (2/2)
    ported fom here: http://gerrit.aokp.co/#/c/12305/
    
    Thanks to Steve Spear
    
    Change-Id: Ibd978f0516521579e5339f21ac81a360740358e8

--- src/com/android/settings/SettingsPreferenceFragment.java
+++ src/com/android/settings/SettingsPreferenceFragment.java
@@ -54,6 +54,9 @@ public class SettingsPreferenceFragment extends PreferenceFragment implements Di
     //Needed for Lockscreen Notifications
     protected Context mContext;
 
+    // Need to use AOKP Custom system animation
+    protected ContentResolver mContentRes; 
+
     // Cache the content resolver for async callbacks
     private ContentResolver mContentResolver;
 
@@ -62,6 +65,9 @@ public class SettingsPreferenceFragment extends PreferenceFragment implements Di
         super.onCreate(icicle);
 
         mContext = getActivity().getApplicationContext();
+
+	// Need to use AOKP Custom system animation
+        mContentRes = getActivity().getContentResolver(); 
 
         // Prepare help url and enable menu if necessary
         int helpResource = getHelpResource();
@@ -339,4 +345,9 @@ public class SettingsPreferenceFragment extends PreferenceFragment implements Di
 
         return true;
     }
+
+    // Need to AOKP Custom system animation
+    public void setTitle(int resId) {
+        getActivity().setTitle(resId);
+    }  
 }

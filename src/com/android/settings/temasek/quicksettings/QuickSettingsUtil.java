SlimRoms/packages_apps_Settings

QS: add theme switcher tile (TRDS) 2/2
this adds a theme switcher tile

normal click: turn on/off dark/light theme
longpress: switch between the auto modes (manual/light sensor/twilight)

Change-Id: I5f4bb146e240bf31b6486aeff85b0828ee6fbb09

src/com/android/settings/slim/quicksettings/QuickSettingsUtil.java
@@ -42,6 +42,7 @@ import static com.android.internal.util.slim.QSConstants.TILE_SCREENTIMEOUT;
 import static com.android.internal.util.slim.QSConstants.TILE_SETTINGS;
 import static com.android.internal.util.slim.QSConstants.TILE_SLEEP;
 import static com.android.internal.util.slim.QSConstants.TILE_SYNC;
+import static com.android.internal.util.slim.QSConstants.TILE_THEME;
 import static com.android.internal.util.slim.QSConstants.TILE_TORCH;
 import static com.android.internal.util.slim.QSConstants.TILE_USBTETHER;
 import static com.android.internal.util.slim.QSConstants.TILE_USER;
@@ -151,6 +152,9 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_REBOOT, R.string.title_tile_reboot,
                 "com.android.systemui:drawable/ic_qs_reboot"));
+        registerTile(new QuickSettingsUtil.TileInfo(
+                TILE_THEME, R.string.title_tile_theme,
+                "com.android.systemui:drawable/ic_qs_theme_manual"));
     }
 
     private static void registerTile(QuickSettingsUtil.TileInfo info) {

terciocarvalho/android_packages_apps_Settings

Conflicts:
	src/com/android/settings/temasek/quicksettings/QuickSettingsUtil.java

src/com/android/settings/temasek/quicksettings/QuickSettingsUtil.java
@@ -47,6 +47,7 @@ import static com.android.internal.util.temasek.QSConstants.TILE_SCREENTIMEOUT;
 import static com.android.internal.util.temasek.QSConstants.TILE_SETTINGS;
 import static com.android.internal.util.temasek.QSConstants.TILE_SLEEP;
 import static com.android.internal.util.temasek.QSConstants.TILE_SYNC;
+import static com.android.internal.util.temasek.QSConstants.TILE_THEME;
 import static com.android.internal.util.temasek.QSConstants.TILE_TORCH;
 import static com.android.internal.util.temasek.QSConstants.TILE_USBTETHER;
 import static com.android.internal.util.temasek.QSConstants.TILE_USER;
@@ -166,6 +167,9 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_CONTACT, R.string.title_tile_contact,
                 "com.android.systemui:drawable/ic_qs_default_user"));
+        registerTile(new QuickSettingsUtil.TileInfo(
+                TILE_THEME, R.string.title_tile_theme,
+                "com.android.systemui:drawable/ic_qs_theme_manual"));
     }
 
     private static void registerTile(QuickSettingsUtil.TileInfo info) {
91d70be11cfa25f26ae8cae63beb5c7ef7e48d3f
Jubakuba <Jubakuba@gmail.com>
2014-03-31 12:32:15 -0300

    Settings: disable keyguard widget frame (2/2)
    Change-Id: Ia18c66dc32b65a2e35807bf643e8d3235d4a0a58

--- src/com/android/settings/cyanogenmod/LockscreenInterface.java
+++ src/com/android/settings/cyanogenmod/LockscreenInterface.java
@@ -76,6 +76,7 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
     private static final String KEY_SEE_THROUGH = "see_through";
     private static final String KEY_BLUR_RADIUS = "lockscreen_blur_radius";
     private static final String LOCK_BEFORE_UNLOCK = "lock_before_unlock";
+    private static final String KEY_DISABLE_FRAME = "lockscreen_disable_frame";
     private static final String LOCKSCREEN_BACKGROUND = "lockscreen_background";
     private static final String WALLPAPER_NAME = "lockscreen_wallpaper";
     private static final String LOCKSCREEN_BACKGROUND_STYLE = "lockscreen_background_style";
@@ -99,6 +100,7 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
     private CheckBoxPreference mSeeThrough;
     private SeekBarPreferenceCHOS mBlurRadius;
     private CheckBoxPreference mLockBeforeUnlock;
+    private CheckBoxPreference mDisableFrame;
 
     private ChooseLockSettingsHelper mChooseLockSettingsHelper;
     private LockPatternUtils mLockUtils;
@@ -122,6 +124,9 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
         // Find preferences
         mEnableKeyguardWidgets = (CheckBoxPreference) findPreference(KEY_ENABLE_WIDGETS);
         mEnableCameraWidget = (CheckBoxPreference) findPreference(KEY_ENABLE_CAMERA);
+
+        // Keyguard widget frame
+        mDisableFrame = (CheckBoxPreference) findPreference(KEY_DISABLE_FRAME);
 
         mBatteryStatus = (ListPreference) findPreference(KEY_BATTERY_STATUS);
         if (mBatteryStatus != null) {
@@ -210,6 +215,12 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
             mBatteryStatus.setValueIndex(batteryStatus);
             mBatteryStatus.setSummary(mBatteryStatus.getEntries()[batteryStatus]);
         }
+
+        if (mDisableFrame != null) {
+            mDisableFrame.setChecked(Settings.System.getInt(getContentResolver(),
+                    Settings.System.LOCKSCREEN_WIDGET_FRAME_ENABLED, 0) == 1);
+            mDisableFrame.setOnPreferenceChangeListener(this);
+        }
     }
 
     @Override
@@ -258,6 +269,11 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
             int color = ColorPickerPreference.convertToColorInt(hex);
             Settings.System.putInt(getContentResolver(),
                     Settings.System.LOCKSCREEN_BACKGROUND_COLOR, color);
+            return true;
+        } else if (preference == mDisableFrame) {
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.LOCKSCREEN_WIDGET_FRAME_ENABLED,
+                    (Boolean) objValue ? 1 : 0);
             return true;
         }
 
